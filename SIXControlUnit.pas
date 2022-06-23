JobstTechnologies/JT-Driver-Sensing

commit de2c075413eb49b1acc026e2733816ce2b306a3a

add context menu entry to autoscale
- also fix further display issue

SIXControlUnit.pas
@@ -1155,8 +1155,11 @@ begin
      as TLineSeries).LinePen.Width:= 1;
    MainForm.SIXTempValues.LinePen.Width:= 1;
   end;
-  // zoom back to normal
+  // zoom back to normal and enable auto scale
   MainForm.SIXCH.ZoomFull;
+  MainForm.AutoscaleMI.Enabled:= true;
+  MainForm.SIXCH.Extent.UseXMax:= true;
+  MainForm.SIXCH.Extent.UseXMin:= true;
  end
  // if checked
  else
@@ -1171,6 +1174,11 @@ begin
   // for the x-axis also te extent must be set
   MainForm.SIXCH.Extent.UseXMax:= false;
   MainForm.SIXCH.Extent.UseXMin:= false;
+  // disable auto scale context menu item
+  MainForm.AutoscaleMI.Enabled:= false;
+  // Under ome unfreproducible circumstances the scall range is not taken
+  // into account. Therefore assure this by setting it again.
+  MainForm.ChartLiveView.ViewportSize:= MainForm.ScrollIntervalFSE.Value;
 
   // info: we cannot go back to LinePen width 2 here because this would have an
   // immediate effect. Thus first do it after the next scrolling occurs.

JTDriverSensingMain.lfm
@@ -10328,6 +10328,10 @@ object MainForm: TMainForm
       Caption = 'Time in days'
       OnClick = TimeDayMIClick
     end
+    object AutoscaleMI: TMenuItem
+      Caption = 'Auto scale'
+      OnClick = AutoscaleMIClick
+    end
   end
   object ChartLiveView: TChartLiveView
     Chart = SIXCH

JTDriverSensingMain.pas
@@ -66,6 +66,7 @@ type
     ChartAxisTransformTime: TChartAxisTransformations;
     GlucoseAvailChanL: TLabel;
     ChangeSensorDataFileMI: TMenuItem;
+    AutoscaleMI: TMenuItem;
     UseCalibGB: TGroupBox;
     HasNoPumpsCB: TCheckBox;
     LoadSensorDataMI: TMenuItem;
@@ -797,6 +798,7 @@ type
     procedure AnOutputOf4CBContextPopup(Sender: TObject; MousePos{%H-}: TPoint;
       var Handled: Boolean);
     procedure AppearanceXBBClick(Sender: TObject);
+    procedure AutoscaleMIClick(Sender: TObject);
     procedure CalibrateTBChange(Sender: TObject);
     procedure ChangeBackColorMIClick(Sender: TObject);
     procedure ChangeSensFileMIClick(Sender: TObject);
@@ -1869,6 +1871,17 @@ end;
 procedure TMainForm.AppearanceXBBClick(Sender: TObject);
 begin
  SIXControl.SCAppearanceXBBClick(Sender);
+end;
+
+procedure TMainForm.AutoscaleMIClick(Sender: TObject);
+begin
+ SIXCH.AxisList[0].Range.UseMax:= False;
+ SIXCH.AxisList[0].Range.UseMin:= False;
+ SIXCH.BottomAxis.Range.UseMax:= false;
+ SIXCH.BottomAxis.Range.UseMin:= false;
+ // for the x-axis also te extent must be set
+ SIXCH.Extent.UseXMax:= false;
+ SIXCH.Extent.UseXMin:= false;
 end;
 
 procedure TMainForm.CalibrateTBChange(Sender: TObject);

commit 51d73b04dd04fb9a122e1155becfe09939ed5a7f

fix display regression introduced with commit de2c07541
- occurred when turning off scrolling view

JTDriverSensing.lpi
@@ -22,10 +22,10 @@
     <VersionInfo>
       <UseVersionInfo Value="True"/>
       <MinorVersionNr Value="9"/>
-      <RevisionNr Value="9"/>
-      <BuildNr Value="9"/>
+      <RevisionNr Value="10"/>
+      <BuildNr Value="1"/>
       <Attributes pvaPreRelease="True"/>
-      <StringTable CompanyName="Jobst Technologies" FileDescription="Program to control CCP1 pumps and to sense SIX biosensors" LegalCopyright="Jobst Technologies GmbH" ProductName="JT DriverSensing" ProductVersion="0.99.9"/>
+      <StringTable CompanyName="Jobst Technologies" FileDescription="Program to control CCP1 pumps and to sense SIX biosensors" LegalCopyright="Jobst Technologies GmbH" ProductName="JT DriverSensing" ProductVersion="0.99.10"/>
     </VersionInfo>
     <BuildModes Count="3">
       <Item1 Name="Default" Default="True"/>

SIXControlUnit.pas
@@ -1157,8 +1157,8 @@ begin
   // zoom back to normal and enable auto scale
   MainForm.SIXCH.ZoomFull;
   MainForm.AutoscaleMI.Enabled:= true;
-  MainForm.SIXCH.Extent.UseXMax:= true;
-  MainForm.SIXCH.Extent.UseXMin:= true;
+  MainForm.SIXCH.BottomAxis.Range.UseMax:= false;
+  MainForm.SIXCH.BottomAxis.Range.UseMin:= false;
  end
  // if checked
  else